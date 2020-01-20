053bc5a705c3ced1d306049c045e2af7cc0ba686
Juan <juank153021@gmail.com>
2020-01-20 12:10:15 -0500

    20/01/20

---
+++ php_udemy/charper_5_poo/clases_52.php
@@ -0,0 +1,33 @@
+<?php
+
+//Esta es una clase que se puede definir como una plantilla con propiedades y métodos
+class persona {
+    //Estos las propiedades
+     public $nombre;
+     public $edad;
+     public $pais;
+     
+     //Esta es el método de una clase 
+     public function show_info() {
+         echo 'Hola mundo!!<br>';
+     }
+     
+}
+
+$juank = new persona;
+
+$juank->nombre = 'JuanK';
+$juank->edad = 34;
+$juank->pais = 'Colombia';
+
+$juank->show_info();
+// echo $juank->nombre . '<br>';
+// echo $juank->edad . '<br>';
+// echo $juank->pais . '<br>';
+
+$alejo = new persona;
+
+$alejo->nombre = 'Monik';
+$alejo->edad = 29;
+$alejo->pais = 'Mexico';
+
