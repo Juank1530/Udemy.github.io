c95b46b976cbafcc888aace3e35c7458a173df9d
Juan <juank153021@gmail.com>
2020-01-26 04:13:17 -0500

    Fix mysqli
    Se arregla error de mysqli

--- php_udemy/charper_8_php_DB/mysql_connect_old_71.php
+++ php_udemy/charper_8_php_DB/mysql_connect_old_71.php
@@ -2,10 +2,33 @@
 
 
 
-$mysqli = mysqli_connect("localhost", "Juank1530", "JuanK1530", "udemy");
 
-print_r($mysqli);
 
+
+$con = mysqli_connect( "localhost" , "Juank" , "" , "test");
+
+print_r($con);
+
+
+// // Check connection
+// if (mysqli_connect_errno())
+// {
+//     echo "Failed to connect to MySQL: " . mysqli_connect_error();
+// }
+
+// mysqli_close($con);
+
+// echo "php end";
+
+
+
+
+
+// $mysqli = mysqli_connect("localhost", "Juank", "", "test");
+
+// print_r($mysqli);
+
+echo 'Hello!!';
 // // Esta es la manera antigua de conectarse y es necesario conocerla porque aun las páginas antiguas lo utilizan
 // // Establecer la conexión en una variable
 // $conexion = mysql_connect('127.0.0.1', 'Juank1530', '') or die();
