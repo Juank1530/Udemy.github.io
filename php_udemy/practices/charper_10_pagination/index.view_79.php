7e28f8d722eae3a53774168f91c8513c038aefaf
Juank1530 <Juank15021@gmail.com>
2020-02-18 23:32:50 -0500

    18/02/20_end_php

--- php_udemy/practices/charper_10_pagination/index.view_79.php
+++ php_udemy/practices/charper_10_pagination/index.view_79.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="es">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -7,33 +8,46 @@
     <link href="https://fonts.googleapis.com/css?family=Lato:300,400,700,900|Oswald&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="styles_79.css">
 </head>
+
 <body>
 
-<div class="contenedor">
-    <h1>Árticulos</h1>
-    <section class="articulos">
-        <!--Ejemlpos de emmet ul#nav>li.item$*4>a{Item$} -->
-        <!-- ul>li$*4{$ - Lorem ipsum dolor sit amet consectetur.} -->
-        <ul>
-            <li>1 - Lorem ipsum dolor sit amet consectetur.</li>
-            <li>2 - Lorem ipsum dolor sit amet consectetur.</li>
-            <li>3 - Lorem ipsum dolor sit amet consectetur.</li>
-            <li>4 - Lorem ipsum dolor sit amet consectetur.</li>
-        </ul>
-    </section>
+    <div class="contenedor">
+        <h1>Árticulos</h1>
+        <section class="articulos">
+            <!--Ejemlpos de emmet ul#nav>li.item$*4>a{Item$} -->
+            <!-- ul>li$*4{$ - Lorem ipsum dolor sit amet consectetur.} -->
+            <ul>
+                <?php foreach ($articulos as $articulo) : ?>
+                    <li><?php echo $articulo['id'] . '.- ' . $articulo['articulo'] ?></li>
+                <?php endforeach; ?>
 
-    <!-- secticon.paginacion>ul>li>a*4{$} -->
-    <secticon class="paginacion">
-        <ul>
-            <li class="disabled">&laquo;</li>
-            <li class="active"><a href="#">1</a></li>
-            <li><a href="#">2</a></li>
-            <li><a href="#">3</a></li>
-            <li><a href="#">4</a></li>
-            <li><a href="#">&raquo;</a></li>
-        </ul>
-    </secticon>
-</div>
-    
+            </ul>
+        </section>
+
+        <!-- secticon.paginacion>ul>li>a*4{$} -->
+        <secticon class="paginacion">
+            <ul>
+                <?php if($pagina == 1): ?>
+                    <li class="disabled">&laquo;</li>
+                <?php else: ?>
+                <?php endif; ?>
+                
+                	<?php if(pagina == 1): ?>
+                    <?php else: ?>
+                    <?php endif; ?>
+                
+
+                    
+                <!-- 
+                <li class="active"><a href="#">1</a></li>
+                <li><a href="#">2</a></li>
+                <li><a href="#">3</a></li>
+                <li><a href="#">4</a></li>
+                <li><a href="#">&raquo;</a></li> -->
+            </ul>
+        </secticon>
+    </div>
+
 </body>
+
 </html>
--- php_udemy/practices/charper_10_pagination/index_79.php
+++ php_udemy/practices/charper_10_pagination/index_79.php
@@ -1,17 +1,60 @@
 <?php
 
+
+
+try {
+    $conection = new PDO('mysql:host=localhost;dbname=udemy', 'juank', '');
+} catch (PDOException $e) {
+    echo 'Error: ' . $e->getMessage();
+    die();
+}
+
+// Se verifica si la variable está seteada, si lo está se obtiene su valor 
+// entero y si no lo está se pone opr defceto 1
+$pagina = isset($_GET['pagina']) ? (int)$_GET['pagina'] : 1;
+
+// Se defini la cantidad de post que se van a mostrar en cada pagina  
+$postPorPagina = 5;
+
+// Se determina el número de la página en la cual nos encontramos par determinar 
+// que elementos de la base de van a mostrar 
+$inicio = ($pagina > 1) ? ($pagina * $postPorPagina - $postPorPagina) : 0;
+
+// Se prepara la conexión, se utiliza SQL_CALC_FOUND_ROWS para saber cuantas filas hay 
+// y se le indica con LIMIT $inicio, $postPorPagina quye muestre determinado número de registros
+// y desde donde empezar y hasta donde terminar      
+$articulos = $conection->prepare("
+    SELECT SQL_CALC_FOUND_ROWS
+    *
+    FROM
+    paginacion
+    LIMIT 
+    $inicio, $postPorPagina
+");
+
+// Se ejecuta la consulta 
+$articulos->execute();
+// Se llaman todos los articulos de la tabla con fetchAll();
+$articulos = $articulos->fetchAll();
+
+// Se valida si la página que se indica existe y sino se redireje al index  
+if (!$articulos) {
+    header('Location: index_79.php');
+}
+
+// Se consulta cuantas filas hay en la tabla 
+$totalArticulos = $conection->query('
+    SELECT
+    FOUND_ROWS()
+    as
+    total
+');
+
+$totalArticulos = $totalArticulos->fetch()['total'];
+
+// Se obtine el número de paginas que se va a generar, dividiendo el total de articulos en los post por página que se va a mostrar
+// se utuliza ceil(); para rendodear al número entero superior y no tener resultos con decimales   
+$numeroPaginas = ceil($totalArticulos / $postPorPagina);
+
+// Se llama al otro archivo despúes de ejecutar toda la logica del php
 require 'index.view_79.php';
-
-
-
-
-
-
-
-
-
-
-
-
-
-?>
