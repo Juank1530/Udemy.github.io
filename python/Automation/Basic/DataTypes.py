191d9e75f34cefe39314b272a398ee92473f6afa
Juank <juank153021@gmail.com>
2021-01-17 01:49:25 -0500

    Delete files from python repo

--- python/Automation/Basic/DataTypes.py
+++
@@ -1,8 +0,0 @@
-#Data Types
-
-print(type(2))
-print(type(2.0))
-print(type('Hello Word'))
-print(type(True))
-
-
--- python/Automation/Basic/InputData.py
+++
@@ -1,4 +0,0 @@
-variable1 = int(input('Input a number'))
-variable2 = int(input('Input a number again'))
-
-print('Result' , variable1 + variable2)
